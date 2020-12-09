e6401f31d9699eaa0594bde38fc9bf6fde0ed5db
elgaridhomaulana <elgaridhomaulana@gmail.com>
2020-12-09 13:50:48 +0700

    updating data realtime firebase

--- FirebaseDB/index.js
+++ FirebaseDB/index.js
@@ -17,64 +17,111 @@ const xAxisGroup = graph.append('g')
 	.attr('transform', `translate(0, ${graphHeight})`);
 const yAxisGroup = graph.append('g');
 
-db.collection('dishes').get().then(
-	res => {
 
-		var data = [];
-		res.docs.forEach(doc => {
-			data.push(doc.data())
-		})
+// sclaes
+const y = d3.scaleLinear()
+	.range([graphHeight, 0]);
 
-		const min = d3.min(data, d => d.orders);
-		const max = d3.max(data, d => d.orders);
-
-		const y = d3.scaleLinear()
-			.domain([0, max])
-			.range([graphHeight, 0]);
-
-		const x = d3.scaleBand()
-			.domain(data.map(item => item.name))
-			.range([0, 500])
-			.paddingInner(0.2)
-			.paddingOuter(0.2);
-
-		// join the data to rects
-		const rects = graph.selectAll('rect')
-			.data(data)
-
-		rects.attr('width', x.bandwidth)
-			.attr('height', d => graphHeight - y(d.orders))
-			.attr('fill', 'orange')
-			.attr('x', d => x(d.name))
-			.attr('y', d => y(d.orders))
-			.attr('stroke', 'whhite')
-			.attr('stroke-width', '2');
-
-		rects.enter()
-			.append('rect')
-			.attr('width', x.bandwidth)
-			.attr('height', d => graphHeight - y(d.orders))
-			.attr('fill', 'orange')
-			.attr('x', d => x(d.name))
-			.attr('y', d => y(d.orders))
-			.attr('stroke', 'white')
-			.attr('stroke-width', '2');
+const x = d3.scaleBand()
+	.range([0, 500])
+	.paddingInner(0.2)
+	.paddingOuter(0.2);
 
 
-		// create and call the axes
-		const xAxis = d3.axisBottom(x);
-		const yAxis = d3.axisLeft(y).ticks(5)
-			.tickFormat(d => d + ' orders');
+// create and call the axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y).ticks(5)
+	.tickFormat(d => d + ' orders');
 
-		xAxisGroup.call(xAxis)
-		yAxisGroup.call(yAxis)
+// update x and y axis text
+xAxisGroup.selectAll('text')
+	.attr('transform', 'rotate(-40)')
+	.attr('text-anchor', 'end')
+	.attr('fill', 'black')
 
-		xAxisGroup.selectAll('text')
-			.attr('transform', 'rotate(-40)')
-			.attr('text-anchor', 'end')
-			.attr('fill', 'black')
+yAxisGroup.selectAll('text')
+	.attr('fill', 'black')
 
-		yAxisGroup.selectAll('text')
-			.attr('fill', 'black')
-	}
-)
+// update function
+const update = (data) => {
+	
+	const max = d3.max(data, d => d.orders);
+
+	// updating scale domains
+	y.domain([0, max])
+	x.domain(data.map(item => item.name))
+
+
+	// join the data to rects
+	const rects = graph.selectAll('rect')
+		.data(data)
+
+	// remove exit selection
+	rects.exit().remove()
+
+	// update current shapes in dom
+	rects.attr('width', x.bandwidth)
+		.attr('height', d => graphHeight - y(d.orders))
+		.attr('fill', 'orange')
+		.attr('x', d => x(d.name))
+		.attr('y', d => y(d.orders))
+	
+	// append the enter selectio in the DOM
+	rects.enter()
+		.append('rect')
+		.attr('width', x.bandwidth)
+		.attr('height', d => graphHeight - y(d.orders))
+		.attr('fill', 'orange')
+		.attr('x', d => x(d.name))
+		.attr('y', d => y(d.orders))
+  
+	// call axes
+	xAxisGroup.call(xAxis)
+	yAxisGroup.call(yAxis)
+
+};
+
+// get data from firestore non realtime
+// db.collection('dishes').get().then(
+// 	res => {
+// 		var data = [];
+// 		res.docs.forEach(doc => {
+// 			data.push(doc.data())
+		
+// 		update(data);
+		
+// 		d3.interval(() => {
+// 			data[0].orders += 50
+// 			// update(data);
+// 		}, 1000)
+// 		});
+
+// 	}
+// );
+
+var data = [];
+// get data from firestore realtime
+db.collection('dishes').onSnapshot(res => {
+	
+	res.docChanges().forEach(change => {
+
+		const doc = {...change.doc.data(), id: change.doc.id};
+
+		switch (change.type) {
+			case 'added':
+				data.push(doc);
+				break;
+			case 'modified':
+				const index = data.findIndex(item => item.id == doc.id);
+				data[index] = doc;
+				break;
+			case 'removed':
+				data = data.filter(item => item.id !== doc.id);
+				break;
+			default:
+				break;
+		}
+	});
+
+	update(data)
+})
