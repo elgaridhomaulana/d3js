9571d7b4189716bf31a94d4efd1b131a6cec92ea
elgaridhomaulana <elgaridhomaulana@gmail.com>
2020-12-09 09:03:22 +0700

    add comment to enter selection

--- Data D3/index.js
+++ Data D3/index.js
@@ -7,12 +7,20 @@ const data = [
 
 const svg = d3.select('svg')
 
+// join dengan data
 const rect = svg.selectAll('rect')
 	.data(data)
-	.attr('width', d => d.width)
+
+// menambahkan attribut kepada rect yang sudah terdapat di dalam DOM
+rect.attr('width', d => d.width)
 	.attr('height', d => d.height)
 	.attr('fill', d => d.fill);
 
+// enter selection
+// apabila element data lebih banyak dibandingkan dengan data element yang terdapat di DOM
+// maka d3 akan mendeteksi hal tersebut dan akan memasukan ke dalam virtual element
+// untuk memasukan virtual element ke dalam DOM kita dapat mengunakan enter selection
+// perhatikan dalam enter selection terdapat append yang mendefinisikan element apa yang ingin ditambahkan ke dom dengan data yang ada
 rect.enter()
 	.append('rect')
 	.attr('width', d => d.width)
